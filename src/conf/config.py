84968a843d685a007554d5bfdefa3b7a06e4d773
Siracenco Serghei <108734530+SiracencoSerghei@users.noreply.github.com>
2024-04-27 19:12:24 +0200

    settings + Home page

--- src/conf/config.py
+++ src/conf/config.py
@@ -1,16 +1,40 @@
 #from dotenv import load_dotenv
+from os import environ
+from pathlib import Path
+from dotenv import load_dotenv
+
 from pydantic_settings import BaseSettings
 
-#load_dotenv()
+BASE_PATH_PROJECT = Path(__file__).resolve().parent.parent
+# print(f"{BASE_PATH_PROJECT=}")
+BASE_PATH = BASE_PATH_PROJECT.parent
+# print(f"{BASE_PATH=}")
+load_dotenv(BASE_PATH.joinpath(".env"))
+APP_ENV = environ.get("APP_ENV")
+# print(f"{APP_ENV=}")
 
 
 class Settings(BaseSettings):
+    APP_NAME: str = "myapp"
+    APP_HOST: str = "0.0.0.0"
+    APP_PORT: int = 8000
+
     DATABASE_HOST: str = 'localhost'
     DATABASE_USER: str = 'postgres'
     DATABASE_PASSWORD: str = 'postgres'
     DATABASE_NAME: str = 'postgres'
     DB_URL: str = "postgresql+asyncpg://postgres:postgres@localhost:5432/postgres"
 
+    REDIS_DOMAIN: str = "0.0.0.0"
+    REDIS_PORT: int = 6379
+
+    CLOUDINARY_NAME: str = "some_name"
+    CLOUDINARY_API_KEY: str = "0000000000000"
+    CLOUDINARY_API_SECRET: str = "some_secret"
+
+    rate_limiter_times: int = 2
+    rate_limiter_seconds: int = 5
+    STATIC_DIRECTORY: str = str(BASE_PATH_PROJECT.joinpath("static"))
     class Config:
         extra = "ignore"
         env_file = ".env"
--- src/database/db.py
+++ src/database/db.py
@@ -1,12 +1,17 @@
 import contextlib
+import logging
 
 from sqlalchemy import create_engine
 from sqlalchemy import URL
 from sqlalchemy.ext.asyncio import async_sessionmaker
 from sqlalchemy.ext.asyncio import AsyncEngine
 from sqlalchemy.ext.asyncio import create_async_engine
+import redis.asyncio as redis
 
 from src.conf.config import config
+from src.conf.config import config
+logger = logging.getLogger(f"{config.APP_NAME}.{__name__}")
+
 
 connection_string = URL.create(
     'postgresql',
@@ -45,6 +50,49 @@ class DataBaseSessionManager:
 sessionmanager = DataBaseSessionManager(config.DB_URL)
 
 
-async def get_db():
-    async with sessionmanager.session() as session:
-        yield session
+def get_db():
+    db = DBSession()
+    try:
+        yield db
+    except SQLAlchemyError as err:
+        print("SQLAlchemyError:", err)
+        db.rollback()
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(err))
+    finally:
+        db.close()
+
+
+def create_redis():
+    return redis.ConnectionPool(
+        host=config.REDIS_DOMAIN,
+        port=config.REDIS_PORT,
+        db=0,
+        decode_responses=False,
+    )
+
+
+def get_redis() -> redis.Redis | None:
+    # Here, we re-use our connection pool
+    # not creating a new one
+    try:
+        logger.debug("get_redis connection_pool")
+        if redis_pool:
+            connection = redis.Redis(connection_pool=redis_pool)
+            return connection
+        logger.debug("get_redis connection_pool None")
+    except:
+        logger.debug("get_redis except")
+
+
+async def check_redis() -> bool | None:
+    try:
+        logger.debug("check_redis")
+        r: redis.Redis | None = get_redis()
+        if r:
+            return await r.ping()
+    except Exception:
+        logger.debug("check_redis fail")
+        return None
+
+
+redis_pool: redis.ConnectionPool = create_redis()
