1eb6442a6a0e5e86a47dd1f6a3192809612e25cb
VladSkopenko <skopirka2k17@gmail.com>
2024-04-30 00:16:38 +0300

    Added doc for comments

--- src/routes/comments.py
+++ src/routes/comments.py
@@ -10,20 +10,37 @@ from src.database.models import User
 from src.repository.comments import create_comment
 from src.repository.comments import delete_comment
 from src.repository.comments import edit_comment
-from src.schemas.comments import CommentSchema,CommentResponseSchema
+from src.schemas.comments import CommentResponseSchema
+from src.schemas.comments import CommentSchema
 from src.services.auth import auth_service
 
 router = APIRouter(prefix="/comments", tags=["comments"])
 
 
-@router.post("/create", response_model=CommentResponseSchema,
-             status_code=status.HTTP_201_CREATED)
+@router.post(
+    "/create", response_model=CommentResponseSchema, status_code=status.HTTP_201_CREATED
+)
 async def create_comment_route(
-        image_id: int,
-        body: CommentSchema,
-        db: AsyncSession = Depends(get_db),
-        current_user: User = Depends(auth_service.get_current_user),
+    image_id: int,
+    body: CommentSchema,
+    db: AsyncSession = Depends(get_db),
+    current_user: User = Depends(auth_service.get_current_user),
 ):
+    """
+    The create_comment_route function creates a comment for an image.
+        The function takes in the following parameters:
+            - image_id: int, the id of the image to create a comment for.
+            - body: CommentSchema, which is used to validate and deserialize data sent by users.
+                It contains two fields (text and user_id) that are required when creating comments.
+
+    :param image_id: int: Get the image id from the url
+    :param body: CommentSchema: Validate the request body
+    :param db: AsyncSession: Pass the database session to the function
+    :param current_user: User: Get the user who is currently logged in
+    :param : Get the image id from the url
+    :return: A comment object
+    :doc-author: Trelent
+    """
     comment = await create_comment(image_id, body, db, current_user)
     if not comment:
         raise HTTPException(status_code=404, detail=detail_message.FILE_NOT_FOUND)
@@ -32,11 +49,26 @@ async def create_comment_route(
 
 @router.put("/edit/{comment_id}/", response_model=CommentResponseSchema)
 async def edit_comment_route(
-        comment_id: int,
-        body: CommentSchema,
-        db: AsyncSession = Depends(get_db),
-        current_user: User = Depends(auth_service.get_current_user),
+    comment_id: int,
+    body: CommentSchema,
+    db: AsyncSession = Depends(get_db),
+    current_user: User = Depends(auth_service.get_current_user),
 ):
+    """
+    The edit_comment_route function allows a user to edit an existing comment.
+        The function takes in the following parameters:
+            - comment_id: int, the id of the comment that is being edited.
+            - body: CommentSchema, a schema containing all of the information needed to update a given file's metadata.
+                This includes fields such as title and description (see models/comment_schema for more details).
+
+    :param comment_id: int: Get the comment id from the url
+    :param body: CommentSchema: Validate the data in the body of a request
+    :param db: AsyncSession: Get the database session
+    :param current_user: User: Get the current user
+    :param : Get the comment_id from the url
+    :return: A comment object
+    :doc-author: Trelent
+    """
     comment = await edit_comment(comment_id, body, db, current_user)
     if not comment:
         raise HTTPException(status_code=404, detail=detail_message.FILE_NOT_FOUND)
@@ -45,9 +77,19 @@ async def edit_comment_route(
 
 @router.delete("/delete/{comment_id}/", response_model=CommentResponseSchema)
 async def delete_comment_route(
-        comment_id: int,
-        db: AsyncSession = Depends(get_db),
-        current_user: User = Depends(auth_service.get_current_user),
+    comment_id: int,
+    db: AsyncSession = Depends(get_db),
+    current_user: User = Depends(auth_service.get_current_user),
 ):
+    """
+    The delete_comment_route function deletes a comment from the database.
+
+    :param comment_id: int: Get the comment_id from the url
+    :param db: AsyncSession: Pass the database session to the function
+    :param current_user: User: Get the current user from the auth_service
+    :param : Get the id of the comment to be deleted
+    :return: The deleted comment
+    :doc-author: Trelent
+    """
     comment = await delete_comment(comment_id, db, current_user)
     return comment
--- tests/repository_comments.py
+++ tests/repository_comments.py
@@ -7,7 +7,8 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.common import detail_message
 from src.database.models import Comment
-from src.database.models import Image, Role
+from src.database.models import Image
+from src.database.models import Role
 from src.database.models import User
 from src.repository.comments import create_comment
 from src.repository.comments import delete_comment
@@ -18,6 +19,14 @@ from src.schemas.comments import CommentSchema
 class TestAsyncContacts(unittest.IsolatedAsyncioTestCase):
 
     def setUp(self) -> None:
+        """
+        The setUp function is called before each test function.
+        It creates a new user and image object, as well as an AsyncMock session object.
+
+        :param self: Represent the instance of the class
+        :return: Nothing
+        :doc-author: Trelent
+        """
         self.user = User(
             id=1,
             nickname="test_user",
@@ -32,6 +41,16 @@ class TestAsyncContacts(unittest.IsolatedAsyncioTestCase):
         )
 
     async def test_create_comment(self):
+        """
+        The test_create_comment function tests the create_comment function in the image.py file.
+        It creates a comment object and then uses that to test if it is an instance of Comment,
+        if its body matches what was created, if it has been refreshed once, committed once and
+        if there is a result.
+
+        :param self: Represent the instance of the object that is passed to the method
+        :return: An instance of the comment class
+        :doc-author: Trelent
+        """
         body = CommentSchema(
             comment="test",
         )
@@ -44,6 +63,18 @@ class TestAsyncContacts(unittest.IsolatedAsyncioTestCase):
         self.assertIsNotNone(result)
 
     async def test_edit_comment(self):
+        """
+        The test_edit_comment function tests the edit_comment function in the comment.py file.
+        The test_edit_comment function is a coroutine that takes in self as an argument and returns nothing.
+        The test_edit_comment function creates a body variable that contains a CommentSchema object with
+        a comment attribute set to &quot;new&quot;. The mocked comment variable is created by calling MagicMock() on
+        the Comment class, which allows us to mock out its scalar one or none method so we can return our own value for it.
+        We then call this method and pass it the return value of creating another Comment object with
+
+        :param self: Access the attributes and methods of the class in python
+        :return: An instance of the comment class
+        :doc-author: Trelent
+        """
         body = CommentSchema(
             comment="new_test",
         )
@@ -64,6 +95,14 @@ class TestAsyncContacts(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(result.image_id, self.image.id)
 
     async def test_edit_comment_permission_error(self):
+        """
+        The test_edit_comment_permission_error function tests the edit_comment function to ensure that it raises a 403 error if the user attempting to edit a comment is not the author of that comment.
+
+
+        :param self: Represent the instance of the object that is passed to a method or function when it is called
+        :return: 403 and permission_error
+        :doc-author: Trelent
+        """
         wrong_user = User(
             id=2,
             nickname="test_user",
@@ -89,6 +128,15 @@ class TestAsyncContacts(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(context.exception.detail, detail_message.PERMISSION_ERROR)
 
     async def test_edit_nonexistent_comment(self):
+        """
+        The test_edit_nonexistent_comment function tests the edit_comment function in the comment.py file.
+        The test_edit_nonexistent_comment function is a coroutine that takes in three arguments: self, body, and session.
+        The test checks to see if an HTTPException is raised when a user tries to edit a comment that does not exist.
+
+        :param self: Represent the instance of the class
+        :return: 404 and file_not_found
+        :doc-author: Trelent
+        """
         body = CommentSchema(
             comment="new_test",
         )
@@ -102,6 +150,17 @@ class TestAsyncContacts(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(context.exception.detail, detail_message.FILE_NOT_FOUND)
 
     async def test_delete_comment(self):
+        """
+        The test_delete_comment function tests the delete_comment function in the comments.py file.
+        The test_delete_comment function creates a mocked admin user, and then creates a mocked comment object that is returned by
+        the scalar_one_or_none method of the session object's execute method. The test then calls the delete comment function with
+        the id of 1, which should return an instance of Comment (which it does). The test also asserts that when called with 1 as its
+        id argument, both session objects' delete and commit methods are called once each.
+
+        :param self: Represent the instance of the object that is using this method
+        :return: An instance of the comment class
+        :doc-author: Trelent
+        """
         admin = User(
             id=1,
             nickname="test_user",
@@ -119,11 +178,21 @@ class TestAsyncContacts(unittest.IsolatedAsyncioTestCase):
         self.session.execute.return_value = mocked_comment
         result = await delete_comment(1, self.session, admin)
         self.assertIsInstance(result, Comment)
-        self.session.delete.assert_called_once_with(mocked_comment.scalar_one_or_none.return_value)
+        self.session.delete.assert_called_once_with(
+            mocked_comment.scalar_one_or_none.return_value
+        )
         self.session.commit.assert_called_once()
         self.assertEqual(result, mocked_comment.scalar_one_or_none.return_value)
 
     async def test_delete_comment_from_wrong_role(self):
+        """
+        The test_delete_comment_from_wrong_role function tests the delete_comment function in the comment.py file.
+        The test is testing that a user with an incorrect role cannot delete a comment from an image.
+
+        :param self: Represent the instance of the class
+        :return: 403 status code and detail message
+        :doc-author: Trelent
+        """
         mocked_comment = MagicMock()
         mocked_comment.scalar_one_or_none.return_value = Comment(
             id=1,
@@ -139,6 +208,15 @@ class TestAsyncContacts(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(context.exception.detail, detail_message.PERMISSION_ERROR)
 
     async def test_delete_nonexistent_comment(self):
+        """
+        The test_delete_nonexistent_comment function tests the delete_comment function in the comments.py file.
+        The test_delete_nonexistent_comment function is a coroutine that takes three arguments: self, session, and user.
+        The test checks to see if an HTTPException is raised when a comment with an id of 1 does not exist in the database.
+
+        :param self: Represent the instance of the class
+        :return: A 404 error message and a detail message
+        :doc-author: Trelent
+        """
         mocked_comment = MagicMock()
         mocked_comment.scalar_one_or_none.return_value = None
         self.session.execute.return_value = mocked_comment
