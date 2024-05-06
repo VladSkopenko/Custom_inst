506fd2b0268ca6abb1bcdac1656b3396130b7e85
Siracenco Serghei <108734530+SiracencoSerghei@users.noreply.github.com>
2024-05-06 19:36:59 +0200

    the changes

--- src/static/client/js/gallery.js
+++ src/static/client/js/gallery.js
@@ -1,3 +1,4 @@
+// gallery.js
 
 const ALL_IMG_URL = `${BASE_URL}/api/images/all`;
 
@@ -10,6 +11,7 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 const imageSize = 'w500';
+
 function render_gallery(photos_data) {
 
     if (photos_data.length === 0) {
@@ -34,13 +36,17 @@ function render_gallery(photos_data) {
         const formattedCreatedDate = `${createdDate.getDate().toString().padStart(2, '0')}-${(createdDate.getMonth() + 1).toString().padStart(2, '0')}-${createdDate.getFullYear()}`;
         const updatedDate = new Date(image.updated_at);
         const formattedUpdatedDate = `${updatedDate.getDate().toString().padStart(2, '0')}-${(updatedDate.getMonth() + 1).toString().padStart(2, '0')}-${updatedDate.getFullYear()}`;
+<<<<<<< Updated upstream
         return `<a href="photo_page.html?imageId=${image.id}&tagsId=${tags_id}&commentsInfo=${comments_info}&rating=${rating}" class="photo-link">
+=======
+        return `<a href="./images_detail.html?imageId=${image.id}&tagsId=${tags_id}&commentsInfo=${comments_info}" class="photo-link">
+>>>>>>> Stashed changes
         <li class="photo" data-id=${image.id}>
   <div class = "photo-image__wrapper">
   <img src="${image.base_url}" alt="${image.title}" data-updated_at=${formattedUpdatedDate} data-qr_url=${image.qr_url} data-transform_url=${image.transform_url}/>
   </div>
   <p class="photo__title">${photoTitle}</p>
-  <p class="photo__user" user-id=${image.user_id} data-user=${image.user}>${image.user.nickname}</p>
+  <p class="photo__user" user-id=${image.user_id} data-user=${image.user}>By ${image.user.nickname}</p>
   <div class="photo__position">
    <p class="photo__description">${image.description}</p>
   <p class="photo__description">|</p>
@@ -53,7 +59,7 @@ function render_gallery(photos_data) {
     }).join('');
     document.querySelector('#js-list').innerHTML = markup;
 
+    document.querySelectorAll('.photo').forEach(photo => {
+        photo.addEventListener('click', handlePhotoClick);
+    });
 }
-
-
-
