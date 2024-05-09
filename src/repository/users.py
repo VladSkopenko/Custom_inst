be606b34f846d8afa0a19826fcbe9a363cbe06eb
VladSkopenko <skopirka2k17@gmail.com>
2024-05-09 22:52:26 +0300

    fix auth"

--- src/repository/users.py
+++ src/repository/users.py
@@ -19,7 +19,11 @@ async def get_admin_exist(db: AsyncSession = Depends(get_db)):
     stmt = select(User).where(User.role == Role.admin)
     user = await db.execute(stmt)
     result = user.scalars().all()
-    return result
+    if result:
+        return True
+    else:
+        return False
+
 
 
 async def get_user_by_email(email: str, db: AsyncSession = Depends(get_db)):
