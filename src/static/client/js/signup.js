389bfb0989b813a90adb9555f93394b5cd5c4ce5
VladSkopenko <skopirka2k17@gmail.com>
2024-05-10 17:59:31 +0300

    повернув все як було no way

--- src/static/client/js/signup.js
+++ src/static/client/js/signup.js
@@ -22,7 +22,7 @@ form?.addEventListener("submit", async (e) => {
 
 
   fetch(
-    "http://photo-bank-by-drujba-drujba-06de47a4.koyeb.app:8000/api/auth/signup", {
+    "http://0.0.0.0:8000/api/auth/signup", {
       method: "POST",
       body: JSON.stringify(data),
       headers: {
